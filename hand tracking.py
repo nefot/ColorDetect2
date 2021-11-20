611bba469e137e1fe6b262808da6f07eddb3fe27
Skryab <nikita1304fsb@gmail.com>
2021-11-20 19:12:12 +0300

    Доделали распознавание ладоней

--- hand tracking.py
+++ hand tracking.py
@@ -1,9 +1,17 @@
-import cv2
+from cv2 import cv2
 import numpy as np
 import mediapipe as mp
 import numpy as np
-from setting import *
 
+myColors = [[25, 72, 133, 40, 255, 255],  # Желтый
+            [68, 115, 0, 121, 255, 255],
+            [68, 115, 0, 121, 255, 255]  # берюзовый
+
+            ]
+myColorsValue = [[0, 255, 255],
+                 [255, 255, 0]
+
+                 ]
 FRAMEWIDTH = 640
 FRAMEHEIGHT = 480
 cap = cv2.VideoCapture(0)
@@ -22,7 +30,11 @@ while True:
     if landMarksList:
         for landmark in landMarksList:
             for id, lm in enumerate(landmark.landmark):
+                h, w, _ = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
 
+                cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+            mpDraw.draw_landmarks(img, landmark, mpHand.HAND_CONNECTIONS)
     cv2.imshow("Orig", img)
     if cv2.waitKey(1) & 0xFF == 27:  # ord('q'):
         break
